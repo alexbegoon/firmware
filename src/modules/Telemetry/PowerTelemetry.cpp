37fc50429831a35c2a795f01bb1eaa05fd0ada53
Alexander Begoon <alex@begoonlab.tech>
2025-05-25 12:49:09 +0200

    Revert "Add MAX17261 sensor"
    This reverts commit a34cfaee333a3211a2ddd11f52746f2e29c3fcc8.

--- src/modules/Telemetry/PowerTelemetry.cpp
+++ src/modules/Telemetry/PowerTelemetry.cpp
@@ -186,8 +186,6 @@ bool PowerTelemetryModule::getPowerTelemetry(meshtastic_Telemetry *m)
         valid = ina3221Sensor.getMetrics(m);
     if (max17048Sensor.hasSensor())
         valid = max17048Sensor.getMetrics(m);
-    if (max17261Sensor.hasSensor())
-        valid = max17261Sensor.getMetrics(m);
 #endif
 
     return valid;
